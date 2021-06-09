a1059d06bf69b1819fbf6a8fab76985337459c3e
Tyler Moroz <moroztyler@gmail.com>
2021-06-09 15:30:58 -0600

    added clear grid function and user grid input

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("button");
+let input = prompt("How many squares per side?", Number());
+let cellWidth = (560 / input);
+let cellHeight = ((592 / input) - 2);
 
 function makeColumns() {
-    for( let i = 0; i <= 15; i++) {
+    for( let i = 0; i < input; i++) {
         let columns = document.createElement('div');
-        columns.style.width = '34.9px';
+        columns.style.width = `${cellWidth}px`;
         columns.style.height = '592px';
         columns.style.margin = '0px';
         columns.style.padding= '0px';
@@ -13,27 +17,32 @@ function makeColumns() {
 }
 
 function makeRows() {
-    for (let i = 0; i <= 15; i++) {
+    for (let i = 0; i < input; i++) {
         let cells = document.createElement('div');
-        cells.style.background = 'white';
-        cells.style.width = "34.9px";
-        cells.style.height = "35px";
+        cells.style.width = `${cellWidth}px`;
+        cells.style.height = `${cellHeight}px`;
         cells.style.border = "1px solid black";
         cells.style.margin = '0px';
         cells.style.padding = '0px';
         cells.addEventListener("mouseover", function(event){
             event.target.style.background = 'black';
         })
+        button.addEventListener('click', function(){
+            cells.style.background = 'white';
+        })
         return cells;
     }
 }
+
+button.addEventListener('click', function(){
+    
+});
  
 function makeGrid() {
-    for (let i = 0; i <= 15; i++) {
+    for (let i = 0; i < input; i++) {
        let columns = container.appendChild(makeColumns());
-       for(let i = 0; i <= 15; i++) {
+       for(let i = 0; i < input; i++) {
         columns.appendChild(makeRows());
-
        }
     }  
 }
