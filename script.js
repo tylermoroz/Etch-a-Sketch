3b907fe1dd2293fde9be231120ebf07b9e86aff7
Tyler Moroz <moroztyler@gmail.com>
2021-06-10 15:33:38 -0600

    allowed users to change the aspect ratio of their grid

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-const container = document.querySelector("#container");
 const button = document.querySelector("button");
-let input = prompt("How many squares per side?", Number());
-let cellWidth = (560 / input);
-let cellHeight = ((592 / input) - 2);
 
-function makeColumns() {
-    for( let i = 0; i < input; i++) {
-        let columns = document.createElement('div');
-        columns.style.width = `${cellWidth}px`;
-        columns.style.height = '592px';
-        columns.style.margin = '0px';
-        columns.style.padding= '0px';
-        columns.style.float = 'left';
-        return columns;
-    }
-}
-
-function makeRows() {
-    for (let i = 0; i < input; i++) {
-        let cells = document.createElement('div');
-        cells.style.width = `${cellWidth}px`;
-        cells.style.height = `${cellHeight}px`;
-        cells.style.border = "1px solid black";
-        cells.style.margin = '0px';
-        cells.style.padding = '0px';
-        cells.addEventListener("mouseover", function(event){
-            event.target.style.background = 'black';
-        })
-        button.addEventListener('click', function(){
-            cells.style.background = 'white';
-        })
-        return cells;
-    }
-}
 
 button.addEventListener('click', function(){
-    
-});
- 
-function makeGrid() {
-    for (let i = 0; i < input; i++) {
-       let columns = container.appendChild(makeColumns());
-       for(let i = 0; i < input; i++) {
-        columns.appendChild(makeRows());
-       }
-    }  
-}
+    const container = document.querySelector("#container");
+    let input = prompt("How many squares per side?", Number());
+    let cellWidth = (560 / input);
+    let cellHeight = ((592 / input) - 2);
 
-makeGrid();
+    function makeColumns() {
+        for( let i = 0; i < input; i++) {
+            let columns = document.createElement('div');
+            columns.style.width = `${cellWidth}px`;
+            columns.style.height = '592px';
+            columns.style.margin = '0px';
+            columns.style.padding= '0px';
+            columns.style.float = 'left';
+            return columns;
+        }
+    }
+    
+    function makeRows() {
+        for (let i = 0; i < input; i++) {
+            let cells = document.createElement('div');
+            cells.style.width = `${cellWidth}px`;
+            cells.style.height = `${cellHeight}px`;
+            cells.style.border = "1px solid black";
+            cells.style.margin = '0px';
+            cells.style.padding = '0px';
+            cells.addEventListener("mouseover", function(event){
+                event.target.style.background = 'black';
+            })
+            button.addEventListener('click', function(){
+                cells.style.background = 'white';
+            })
+            return cells;
+        }
+    }
+    
+    function makeGrid() {
+        for (let i = 0; i < input; i++) {
+           let columns = container.appendChild(makeColumns());
+           for(let i = 0; i < input; i++) {
+            columns.appendChild(makeRows());
+           }
+           button.addEventListener('click', function(){
+            if(makeGrid){
+                columns.remove();
+            }
+        }) 
+        } 
+    }
+    
+    makeGrid();
+    
+
+})
